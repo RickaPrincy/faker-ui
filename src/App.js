5f098953eeb81e1f64e01739a35725b6fea77a6a
RickaPrincy <rckprincy@gmail.com>
2023-04-29 11:47:48 +0300

    Initial Faker UI Project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import "./App.css"
+import "./App.css";
 
 function App(){
-    return <div>
-        <h1>Hello bro ! I'm the best XD !!!!</h1>
-    </div>
+    
+    return (
+        <div>
+            <h1>Hello bro</h1>
+        </div>
+    );
 }
 
 export default App;
